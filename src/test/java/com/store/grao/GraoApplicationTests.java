2653b0308539802c8b8eef22e981f5e797ae67e5
Cenafowzin <digodubeux@gmail.com>
2024-05-13 23:27:01 -0300

    inicializando

---
+++ src/test/java/com/store/grao/GraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.grao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
