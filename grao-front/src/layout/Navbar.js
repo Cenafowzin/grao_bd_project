b3f6e50e97055f11a66bc29a2006d81962f047fc
Diego <dca2@cesar.school>
2024-05-23 21:32:19 -0300

    att navbar

--- grao-front/src/layout/Navbar.js
+++ grao-front/src/layout/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <button className='btn btn-outline-light'> Gerente</button>
+                    <button className='btn btn-outline-light'> Gerente! </button>
 
                 </div>
             </nav>
