472f87be1045f62bfdf19779f9cd3d99784d55ac
Cenafowzin <digodubeux@gmail.com>
2024-05-28 02:55:55 -0300

    logo e login sem navbar

--- grao-front/src/layout/Navbar.js
+++ grao-front/src/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { LojaContext } from '../Loja/LojaContext';
+import grao_logo from '../Images/grao_logo.png'
 
 export default function Navbar({ isAuthenticated, handleLogout }) {
   const { selectedLojaId, selectedLojaBairro } = useContext(LojaContext);
@@ -12,12 +13,17 @@ export default function Navbar({ isAuthenticated, handleLogout }) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#43B02A' }}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/" style={{ color: '#FFF' }}>Grão</Link>
+                <Link className="navbar-brand" to="/" style={{ color: '#FFF' }}>
+                    <img src={grao_logo} alt="Grão" style={{ height: '40px' }} />
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" style={{ color: '#FFF' }} to="/lojas">Relatório</Link>
+                        </li>
                         <li className="nav-item">
                             <Link className="nav-link" style={{ color: '#FFF' }} to="/lojas">Lojas</Link>
                         </li>
